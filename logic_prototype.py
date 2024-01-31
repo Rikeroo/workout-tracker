2c64be7831d0b870a3e13f96f7ca42bf176f9d35
Rik Williamson <38403726+Rikeroo@users.noreply.github.com>
2024-01-31 11:32:36 +0000

    update function for volume calculations

--- logic_prototype.py
+++ logic_prototype.py
@@ -15,18 +15,31 @@ class Workout:
         """
         self.date = date
         self.exercises = exercises
+        self.volume = 0
+        self.update()
     
     def __str__(self):
         exercises_str = ""
         for e in self.exercises:
             exercises_str += f"{e}"
-        return f"{self.date}\n{exercises_str}"
+        return f"Date: {self.date}\nVolume: {self.volume}\n{exercises_str}"
+   
+    def update(self):
+        vol_total = 0
+        for e in self.exercises:
+           vol_total += e.volume
+        self.volume = vol_total
+           
     
+    # def getVolume(self):
+
+# This might not be best practice, as this is just data?    
 class Exercise:
     """
     Each exercise has list of sets with attributes: reps, weight
     e.g. exercise.getSet(x) returns array [set_num, weight, reps]
     """
+
     def __init__(self, name, sets, notes=""):
         """Initialises class with arguments: sets (2d list) and
         notes (string)
@@ -39,12 +52,24 @@ class Exercise:
         self.name = name
         self.sets = sets
         self.notes = notes
+        self.volume = 0
+        self.update()
         
     def __str__(self):
         sets_str = ""
         for s in self.sets:
             sets_str += f"Set: {s[0]}, Weight: {s[1]}, Reps: {s[2]}\n"
         return f"Name: {self.name}\n{sets_str}"
+    
+    def update(self):
+        total = 0
+        for s in self.sets:
+            total += s[1]*s[2]
+        self.volume = total
+    
+    def getVolume(self):
+        self.update()
+        return self.volume
             
             
 test_sets = [[1, 10, 10],
@@ -59,6 +84,9 @@ e2 = Exercise('Test Exercise 2', test_sets)
 print(e1)
 print(e2)
 
+# print(f"Volume: {e1.getVolume()}")
+
 # Put Exercises into a workout
 w1 = Workout(date.today(),[e1,e2])
 print(w1)
+# print(f"Workout Volume: {w1.volume}")
